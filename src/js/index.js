635b9c1b2d2afef32571dd06790d17c510c528cb
zarek <781568228@qq.com>
2019-11-30 23:37:53 +0800

    添加rem适配方案

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,25 @@
 // 放置主要的js问卷
 import '../css/index.less'
+
+
+ //取消默认行为
+ document.addEventListener('touchstart',function(event){
+    event.preventDefault();
+});
+//解决点透事件
+!(function(){
+    var aNodes = document.querySelectorAll('a');
+    for (var i=0;i<aNodes.length;i++) {
+        aNodes[i].addEventListener('touchstart',function(){
+            window.location.href = this.href;
+        })
+    }
+})();
+        
+//rem适配
+!(function(){
+    var styleNode = document.createElement('style');
+    var width = document.documentElement.clientWidth;
+    styleNode.innerHTML = 'html{font-size: '+ width/16 +'px !important;}';
+    document.head.appendChild(styleNode)
+})();
