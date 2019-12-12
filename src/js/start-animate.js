c97a288011d234473760753e5216bf75a67bae11
Chendabaiiii <532323969@qq.com>
2019-12-12 21:59:29 +0800

    fixed：完善开机动画

--- src/js/start-animate.js
+++ src/js/start-animate.js
@@ -15,23 +15,23 @@
         }
         init() {
             console.log(this.$rowOne);
+            let singgle = 600;
             //第一行
-            this.resetStye(this.$rowOne,"row-one-first-change","row-one-second-change",0);
+            this.resetStyle(this.$rowOne,"row-one-first-change","row-one-second-change",0);
             //第二行
-            this.resetStye(this.$rowTwo,"row-two-first-change","row-two-second-change",400);
+            this.resetStyle(this.$rowTwo,"row-two-first-change","row-two-second-change",1 * singgle);
             //第三行
-            this.resetStye(this.$rowThree,"row-three-first-change","row-three-second-change",800);
+            this.resetStyle(this.$rowThree,"row-three-first-change","row-three-second-change",2 * singgle);
             //第四行
-            this.resetStye(this.$rowForth,"row-forth-first-change","row-forth-second-change",1200);
+            this.resetStyle(this.$rowForth,"row-forth-first-change","row-forth-second-change",3 * singgle);
         }
-        resetStye(obj,change1,change2,time) {
+        resetStyle(obj,change1,change2,time) {
             console.log("类名被改了")
             setTimeout(function() {
                 obj.addClass(change1);
                 setTimeout(function() {
                     obj.addClass(change2);
-
-                },800);
+                },900);
             },time)
         }
     }
