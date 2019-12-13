b0541351b7cfd47f20a47e21d98f377fa767837d
zarek <781568228@qq.com>
2019-12-13 09:15:26 +0800

    renew code from origin dev

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-// 放置主要的js文件
-import './rem'
-import '../css/reset.less'
-import '../css/index.less'
-import '../bootstrap-3.3.7-dist/css/bootstrap.css'
-import './zl-index'
-import './mq-production'
-
