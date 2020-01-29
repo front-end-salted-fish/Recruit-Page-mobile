f0067ce8128bda316b7cea1a88a77b945e398c1a
zarek <781568228@qq.com>
2020-01-29 10:14:17 +0800

    featuer:新增表单验证码匹配和防范xss攻击

--- src/js/form.js
+++ src/js/form.js
@@ -1,5 +1,6 @@
 import zlPlane from './zl-plane'
 import rjBanner from './rj-index'
+import filterXSS from 'xss'
 /* $(document).ready(function () {
     　　$('body').height($('body')[0].clientHeight);
     });//解决软键盘覆盖的问题Android---》不知道行不行哈 */
@@ -47,6 +48,8 @@ $("#wf-name").on("blur", nameCheck);//1.名字
 function nameCheck() {
     let reg = /^[\u4e00-\u9fa5]{2,10}$/;//2-10位中文
     let name = $('#wf-name').val();
+    name = filterXSS(name)
+    console.log(name)
     if (!reg.test(name) || name == '') {
         $("#wf-name").css("border", "1px solid red");
         $(".wf-name-span").html("<span class='red-form'>请输入2~10位中文</span>");
@@ -72,6 +75,7 @@ function idCheck() {
 $("#wf-grade").on("blur", gradeCheck);//3.年级专业
 function gradeCheck() {
     let grade = $("#wf-grade").val();
+    grade = filterXSS(grade)
     if (grade == '') {
         $("#wf-grade").css("border", "1px solid red");
         $(".wf-grade-span").html("<span class='red-form'>不能为空！</span>");
@@ -85,6 +89,7 @@ $("#wf-phone").on("blur", phoneCheck);//4.手机
 function phoneCheck() {
     let reg = /^1(3|4|5|6|7|8|9)\d{9}$/;
     let phone = $("#wf-phone").val();
+    phone = filterXSS(phone)
     if (!reg.test(phone) || phone == '') {
         $("#wf-phone").css("border", "1px solid red");
         $(".wf-phone-span").html("<span class='red-form'>请输入正确的手机号码</span>");
@@ -99,6 +104,7 @@ $("#wf-email").on("blur", emailCheck);//5.邮箱
 function emailCheck() {
     let reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
     let email = $('#wf-email').val();
+    email = filterXSS(email)
     if (!reg.test(email) || email == '') {
         $("#wf-email").css("border", "1px solid red");
         $(".wf-email-span").html("<span class='red-form'>请输入正确的邮箱</span>");
@@ -111,6 +117,7 @@ function emailCheck() {
 $("#wf-intro").on("blur", introCheck);//6.自我介绍
 function introCheck() {
     let intro = $("#wf-intro").val();
+    intro = filterXSS(intro)
     if (intro == '') {
         $("#wf-intro").css("border", "1px solid red");
         $(".wf-intro-span").html("<span class='red-form'>不能为空！</span>");
@@ -123,6 +130,7 @@ function introCheck() {
 $("#wf-skills").on("blur", skillsCheck);
 function skillsCheck() {
     let skills = $("#wf-skills").val();
+    skills = filterXSS(skills)
     if (skills == '') {
         $("#wf-skills").css("border", "1px solid red");
         $(".wf-skills-span").html("<span class='red-form'>不能为空！</span>");
@@ -135,6 +143,7 @@ function skillsCheck() {
 $("#wf-cog").on("blur", cogCheck);
 function cogCheck() {
     let cog = $("#wf-cog").val();
+    cog = filterXSS(cog)
     if (cog == '') {
         $("#wf-cog").css("border", "1px solid red");
         $(".wf-cog-span").html("<span class='red-form'>不能为空！</span>");
@@ -142,6 +151,37 @@ function cogCheck() {
     }
     $("#wf-cog").css("border", "");
     $(".wf-cog-span").html("");
+    return true;
+}
+//产生验证码  
+createCode();
+var code; //在全局定义验证码  
+//  var zlFlag = true; //用于解决使用tap事件时触发两次的bug
+function createCode() {
+    code = "";
+    var codeLength = 4; //验证码的长度  
+    var checkCode = document.getElementById("code");
+    var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //随机数  
+    for (var i = 0; i < codeLength; i++) { //循环操作  
+        var index = Math.floor(Math.random() * 36); //取得随机数的索引（0~35）  
+        code += random[index]; //根据索引取得随机数加到code上  
+    }
+    checkCode.value = code; //把code值赋给验证码  
+}
+// 匹配验证码
+function check() {
+    var inputCode = document.getElementById("ctl00_txtcode").value.toUpperCase();
+    if (inputCode == "") {
+        console.log(1)
+        alert("验证码不能为空");
+        return false;
+    } else if (inputCode != code) {
+        alert("验证码输入错误,请重新输入！");
+        createCode(); //刷新验证码  
+        document.getElementById("ctl00_txtcode").value = ""; //清空文本框
+        return false;
+    }
     return true;
 }
 //提交表单
@@ -167,11 +207,18 @@ $("#wf-commit").on('tap', function () {
                     btn.attr('disabled', false);
                 }, 2000); */
     if (nameCheck() && idCheck() && gradeCheck() && phoneCheck() && emailCheck() && introCheck() && skillsCheck() && cogCheck()) {
-        //在这里提交，小飞机 
-        zlPlane();
+        // 判断验证码是否匹配
+        if (!check()) {
+            return false
+        } else {
+            //在这里提交，小飞机 
+            zlPlane();
+        }
     }
-    else console.log("请正确输入信息");
-    //}
+    else {
+        console.log("请正确输入信息");
+    }
+
 
     //
     //关闭弹窗，动画
